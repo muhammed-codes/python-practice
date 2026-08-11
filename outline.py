62965e0bbd7ff13855a07504e0458fe6ed154fd6
muhammed-codes <dimeji.asynctechs@gmail.com>
2026-08-11 15:20:21 +0100

    Add outline and practice scripts for lessons on operators and type checking

---
+++ outline.py
@@ -0,0 +1,26 @@
+##Lesson 3
+#Assignment Operators
+#Arithmetic Operators
+#Combining assignment and arithmetic operators
+#Concatenation
+#Comparison and Boolean Operators
+#Conditional Statements
+#Tenary Operators
+
+##Lesson 4
+#Literal assignments 
+first_name = "John"
+last_name = "Doe"
+print(first_name + " " + last_name) # Concatenation of strings 
+print(type(first_name)) 
+print(type(last_name) == str) # Checking the type of the variable first_name and last_name
+print(isinstance(first_name, str)) # Checking if first_name is an instance of str
+
+#Constructor function
+age = int(input("Enter your age: "))
+level = str(input("Enter your level: "))
+
+#Casting
+birth_month = str(input("Enter your month of birth: "))
+
+#naming conventions for files in python is all lowercase letters with underscores between words. For example, my_file.py is a valid file name.
---
+++ practice.py
@@ -0,0 +1,16 @@
+round (5.678, 2) # the round function rounds a number to a specified number of decimal places. In this case, it rounds 5.678 to 2 decimal places, resulting in 5.68.
+
+initial_value = 10.0
+initial_value += 5.0 # This line adds 5.0 to the initial_value, resulting in 15.0.
+
+initial_value -= 3.24 # This line subtracts 3.24 from the initial_value, resulting in 11.76.
+
+initial_value *= 2.0 # This line multiplies the initial_value by 2.0, resulting in 23.52.
+print(initial_value)
+
+initial_value /= 4.0 # This line divides the initial_value by 4.0, resulting in 5.88.
+print(round(initial_value)) # This line rounds the initial_value to whole numbers, resulting in 6.
+
+
+#Tenary Operators
+print("You are eligible to vote." if initial_value >= 18 else "You are not eligible to vote.") 
