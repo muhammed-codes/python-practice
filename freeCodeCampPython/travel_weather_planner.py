99b210d74292a629e35dc7f7d8704d5e499a3825
muhammed-codes <dimeji.asynctechs@gmail.com>
2026-08-26 07:08:32 +0100

    add travel weatherplanner lab project

---
+++ freeCodeCampPython/travel_weather_planner.py
@@ -0,0 +1,23 @@
+distance_mi = 0
+is_raining = True
+has_bike = True
+has_car = False
+has_ride_share_app = True
+
+if not distance_mi or isinstance(distance_mi, str):
+    print(False)
+elif distance_mi <= 1:
+    if not is_raining:
+        print(True)
+    else:
+        print(False)
+elif distance_mi > 1 and distance_mi <= 6:
+    if has_bike:
+        print(True)
+    else:
+        print(False)
+elif distance_mi > 6:
+    if has_car or has_ride_share_app:
+        print(True)
+    else:
+        print(False)
