14f1cbce7950d40a653d7c764d5d2560ad619f21
muhammed-codes <dimeji.asynctechs@gmail.com>
2026-08-26 22:01:07 +0100

    refactor: replace hardcoded encryption output with variable reference in caesar_cipher.py

--- freeCodeCampPython/caesar_cipher.py
+++ freeCodeCampPython/caesar_cipher.py
@@ -22,7 +22,7 @@ def encrypt(text, shift):
 def decrypt(text, shift):
     return caesar(text, shift, encrypt=False)
 
-encrypted_text = encrypt('freeCodeCamp', 3)
-decrypted_text = decrypt('Pbhentr vf sbhaq va hayvxryl cynprf.', 13)
+encrypted_text = 'Pbhentr vf sbhaq va hayvxryl cynprf.'
+decrypted_text = decrypt(encrypted_text, 13)
 print(encrypted_text)
 print(decrypted_text)
